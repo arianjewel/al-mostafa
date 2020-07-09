28ec91787b77bcbea366e64e082519fee964d4a2
Md. Nur Alam Siddique <35496732+arianjewel@users.noreply.github.com>
2020-07-09 17:32:52 +0600

    carrer single apply button add

--- resources/views/frontend/career/career_single.blade.php
+++ resources/views/frontend/career/career_single.blade.php
@@ -18,7 +18,8 @@
         <div class="container">
            <div class="row">
                <div class="col-md-9">
-                    {!!$career->body!!} 
+                {!!$career->body!!}
+                <a style="background-color: #0072c2; color: white;" class="btn btn-primary" @if($career->url) href="{{route('contact')}}" @endif >Apply Online</a>
                </div>
                @include('frontend.partials.sidebar')
             </div>
